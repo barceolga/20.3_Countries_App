03153a82ba93def28652b907f04a7d0b213d0401
Olga Furmanowska <olgafurmanowska@gmail.com>
2018-07-03 08:43:02 +0200

    Update content for index.js file

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './store/index.js';
+import DevTools from './DevTools.js';
+import { GET_COUNTRIES, getCountries } from './actions/actions-countries.js';
 
 render(
-    <h1>The project initialization</h1>,
+  <Provider store={store}>
+    <div>
+        <h1>The project initialization</h1>
+        <DevTools />
+    </div>
+  </Provider>,
     document.getElementById('root')
 );
+
+store.dispatch(getCountries());
