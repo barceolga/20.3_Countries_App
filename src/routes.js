6dadfda1de538a6d10178b829e49bf930ed0d4df
Olga Furmanowska <olgafurmanowska@gmail.com>
2018-07-04 09:36:11 +0200

    Update router with new route in routes.js file

--- src/routes.js
+++ src/routes.js
@@ -5,11 +5,15 @@ import Home from './presentational/home.component.js';
 import Contact from './presentational/contact.component.js';
 import NotFound from './presentational/not-found.component.js';
 import CountryFlagContainer from './containers/flag-container.component.js';
+import CountryDetailsContainer from './containers/country-details-container.component.js';
 
 export default (
   <Route path='/' component={Navigation}>
       <IndexRoute component={Home}/>
-      <Route path='countries' component={CountryFlagContainer} />
+      <Route path='countries'>
+          <IndexRoute component={CountryFlagContainer} />
+          <Route path='country/:id' component={CountryDetailsContainer} />
+      </Route>
       <Route path='contact' component={Contact} />
       <Route path='*' component={NotFound} />
   </Route>
