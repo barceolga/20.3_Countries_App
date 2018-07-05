42562d446d39d9523aab7364b65745f7458212ee
Olga Furmanowska <olgafurmanowska@gmail.com>
2018-07-05 15:09:38 +0200

    Delete unnecesary case from reducer in countries-reducer.js file

--- src/reducers/countries-reducer.js
+++ src/reducers/countries-reducer.js
@@ -1,4 +1,4 @@
-import { GET_COUNTRIES, GET_COUNTRY, SEARCH_COUNTRIES, SEARCH_CAPITAL, SEARCH_CURRENCY, SEARCH_LANGUAGE, DELETE_COUNTRY, SET_CONTINTENT, DELETE_COUNTRIES } from '../actions/actions-countries.js';
+import { GET_COUNTRY, SEARCH_COUNTRIES, SEARCH_CAPITAL, SEARCH_CURRENCY, SEARCH_LANGUAGE, DELETE_COUNTRY, SET_CONTINTENT, DELETE_COUNTRIES } from '../actions/actions-countries.js';
 import countriesData from '../data/countries.json';
 
 const initialState = {
@@ -9,40 +9,61 @@ const initialState = {
 
 const countriesReducer = function (state = initialState, action) {
   switch (action.type) {
-    case GET_COUNTRIES:
-            return Object.assign({}, state, {countries: state.countries});
+
     case GET_COUNTRY:
-            const selectedCountry = state.countries.find(country => country.id === parseInt(action.id));
+            const selectedCountry = state.countries.find(
+              country => country.id === parseInt(action.id)
+            );
             return Object.assign({}, state, {selectedCountry});
     case SEARCH_COUNTRIES:
-            const foundCountries = state.countries.filter(country => country.name.toLowerCase().includes(action.searchText.toLowerCase()));
+            const foundCountries = state.countries.filter(
+              country => country.name.toLowerCase().includes(action.searchText.toLowerCase())
+            );
             return Object.assign({}, state, {
               visibleCountries: foundCountries,
             });
     case SEARCH_LANGUAGE:
-            const foundByLanguage = state.countries.filter(country => country.languages.toLowerCase().includes(action.searchText.toLowerCase()));
+            const foundByLanguage = state.countries.filter(
+              country => country.languages.toLowerCase().includes(action.searchText.toLowerCase())
+            );
             return Object.assign({}, state, {
               visibleCountries: foundByLanguage
             });
     case SEARCH_CAPITAL:
-            const foundByCapital = state.countries.filter(country => country.capital.toLowerCase().includes(action.searchText.toLowerCase()));
+            const foundByCapital = state.countries.filter(
+              country => country.capital.toLowerCase().includes(action.searchText.toLowerCase())
+            );
             return Object.assign({}, state, {
                 visibleCountries: foundByCapital
             });
     case SEARCH_CURRENCY:
-            const foundByCurrency = state.countries.filter(country => country.currency.toLowerCase().includes(action.searchText.toLowerCase()));
+            const foundByCurrency = state.countries.filter(
+              country => country.currency.toLowerCase().includes(action.searchText.toLowerCase())
+            );
             return Object.assign({}, state, {
                 visibleCountries: foundByCurrency
             });
     case DELETE_COUNTRY:
-            const leftCountries = state.countries.filter(country => country.id !== parseInt(action.id));
-            const leftVisibleCountries = state.visibleCountries.filter(country => country.id !== parseInt(action.id));
-            return Object.assign({}, state, {countries: leftCountries, visibleCountries: leftVisibleCountries});
+            const leftCountries = state.countries.filter(
+              country => country.id !== parseInt(action.id)
+            );
+            const leftVisibleCountries = state.visibleCountries.filter(
+              country => country.id !== parseInt(action.id)
+            );
+            return Object.assign({}, state, {
+              countries: leftCountries, visibleCountries: leftVisibleCountries
+            });
     case SET_CONTINTENT:
-            const continentCountries = state.countries.filter(country => country.continent === action.name);
-            return Object.assign({}, state, {visibleCountries: continentCountries});
+            const continentCountries = state.countries.filter(
+              country => country.continent === action.name
+            );
+            return Object.assign({}, state, {
+              visibleCountries: continentCountries
+            });
     case DELETE_COUNTRIES:
-            return Object.assign({}, state, {countries: [], visibleCountries: []});
+            return Object.assign({}, state, {
+              countries: [], visibleCountries: []
+            });
 
     default:
             return state;
