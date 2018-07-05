14df75b490c9e8776f4dc2cd8d2b5b3601fa57af
Olga Furmanowska <olgafurmanowska@gmail.com>
2018-07-05 07:55:58 +0200

    Add new case in reducer for reducer-countries.js file

--- src/reducers/countries-reducer.js
+++ src/reducers/countries-reducer.js
@@ -1,4 +1,4 @@
-import { GET_COUNTRIES, GET_COUNTRY, SEARCH_COUNTRIES, DELETE_COUNTRY } from '../actions/actions-countries.js';
+import { GET_COUNTRIES, GET_COUNTRY, SEARCH_COUNTRIES, DELETE_COUNTRY, SET_CONTINTENT } from '../actions/actions-countries.js';
 import countriesData from '../data/countries.json';
 
 const initialState = {
@@ -21,6 +21,9 @@ const countriesReducer = function (state = initialState, action) {
             const leftCountries = state.countries.filter(country => country.id !== parseInt(action.id));
             const leftVisibleCountries = state.visibleCountries.filter(country => country.id !== parseInt(action.id));
             return Object.assign({}, state, {countries: leftCountries, visibleCountries: leftVisibleCountries});
+    case SET_CONTINTENT:
+            const continentCountries = state.countries.filter(country => country.continent === action.name);
+            return Object.assign({}, state, {visibleCountries: continentCountries});
 
 
     default:
